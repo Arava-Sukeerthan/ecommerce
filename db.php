6a8b1756d0a4f24178249e79dd144a351a0bc510
Arava-Sukeerthan <asukeertthan@gmail.com>
2025-11-24 23:19:59 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = ""; // default in XAMPP
+$dbname = "ecommerce"; // change if needed
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
